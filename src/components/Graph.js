afeef04258583e4b2bf42aba70ce453d3a75151b
Zulinlo <samm.wu2@gmail.com>
2021-05-17 18:08:51 +1000

    Attempting to import excel and read from it

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,6 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import raw from '../APT.AX.csv';
+// React Libraries
+import * as XLSX from 'xlsx';
 import { Line } from 'react-chartjs-2';
 
 const Graph = () => {
+    const [prices, setPrices] = useState([]);
+
+
+    useEffect(() => {
+        const promise = new Promise((resolve, reject) => {
+            const fileReader = new FileReader();
+            fileReader.readAsArrayBuffer(raw);
+
+            fileReader.onload = e => {
+                const bufferArray = e.target.result;
+
+                const wb = XLSX.read(bufferArray, {type: 'buffer'});
+
+                const wsname = wb.SheetNames[1];
+
+                const ws=wb.Sheets[wsname];
+
+                const data=XLSX.utils.sheet_to_json(ws);
+
+                resolve(data);
+            }
+
+            fileReader.onerror=(error => {
+                reject(error);
+            })
+        })
+
+        promise.then(d => {
+            setPrices(d);
+        })
+    });
+
     return (
         <div>
             <Line
@@ -11,7 +47,7 @@ const Graph = () => {
                             label: 'Share Price',
                             borderColor: 'black',
                             backgroundColor: 'white',
-                            data: [12, 3, 2, 5, 3],
+                            data: {prices},
                         }
                     ],
                 }}
